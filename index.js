ac97fc23cd477819d3f795c6507209f7152d233c
Toyse_Devs <olayonwatoyib05@gmail.com>
2025-06-28 12:07:46 +0100

    I made changes to the navbar to be static then i add header class to the html so i can use it in the js file

--- index.js
+++ index.js
@@ -41,3 +41,12 @@ function type() {
 document.addEventListener("DOMContentLoaded", () => {
     type();
 });
+
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.header');
+  if (window.scrollY > 50) { // Adjust threshold (50px)
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
